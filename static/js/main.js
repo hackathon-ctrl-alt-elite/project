cd0ee43a6dd7c439055bb7e67a9c1ef310a06e05
AlissaPepino <alissapepino@gmail.com>
2025-02-17 10:39:08 -0700

    formatted course display

--- static/js/main.js
+++ static/js/main.js
@@ -14,9 +14,36 @@ document.querySelector("#submit_button").addEventListener('click', () => {
 
         // Display the fetched data
         data.forEach(item => {
+            /*
             let output = document.createElement("p");
             output.textContent = `Title: ${item.title}, Description: ${item.description}, URL: ${item.url}`;
             main.appendChild(output);
+            */
+            
+            // create display card
+            let card = document.createElement("div");
+            card.setAttribute("class","course-display-card");
+
+            // Course Title
+            let courseTitle = document.createElement("h2");
+            courseTitle.setAttribute("class", "card-title");
+            courseTitle.textContent= `${item.title}`;
+
+            // Faculty here
+            let courseDesc = document.createElement("h4");
+            courseDesc.setAttribute("class", "card-desc");
+            courseDesc.textContent= `${item.description}`;
+
+            // Description here
+            let courseURL = document.createElement("p");
+            courseURL.setAttribute("class", "card-url");
+            courseURL.textContent= `${item.url}`;
+
+
+            card.appendChild(courseTitle);
+            card.appendChild(courseDesc); 
+            card.appendChild(courseURL);  
+            main.appendChild(card);
         });
     })
     .catch(error => {
